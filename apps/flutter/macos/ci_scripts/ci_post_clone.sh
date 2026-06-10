8de74edaa646eeb709b8b2cad899f13a79758520
Alex.F <gfreezy@gmail.com>
2026-06-10 11:59:12 +0800

    commit

---
+++ apps/flutter/macos/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Fail this script if any subcommand fails.
+set -e
+
+brew install zig@0.15 go
+
+# The default execution directory of this script is the ci_scripts directory.
+cd $CI_PRIMARY_REPOSITORY_PATH/apps/flutter # change working directory to the root of your cloned repo.
+
+# Install Flutter using git.
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin:/opt/homebrew/opt/zig@0.15/bin"
+export LDFLAGS="-L/opt/homebrew/opt/zig@0.15/lib"
+
+# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
+flutter precache --ios
+
+# Install Flutter dependencies.
+flutter pub get
+
+exit 0
